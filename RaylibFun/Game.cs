6c5bc7bcb2f1f70095b12ca105f142804e473b68
Joshua-Production <joshuaespinosabusiness@outlook.com>
2024-10-25 12:59:22 -0500

    Add project files.

---
+++ RaylibFun/Game.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Raylib_cs;
+namespace RaylibFun
+{
+    internal class Game
+    {
+
+
+
+        public void Run()
+        {
+            Raylib.InitWindow(640, 480, "Game Window");
+            Raylib.SetTargetFPS(60);
+             
+            
+            
+
+            // Player
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Gray);
+
+                
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+
+
+
+
+
+
+
+        }
+    }
+}
